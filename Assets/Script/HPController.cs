3381679274beeb7b42c854e7f3c1ce99d42687c3
ryotaorolab <itour433@yahoo.co.jp>
2023-03-24 11:42:43 +0900

    HPが0以下になるとリスポーンするように、ステージ改良

--- Assets/Script/HPController.cs
+++ Assets/Script/HPController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HPController : MonoBehaviour
 {
@@ -18,7 +19,10 @@ public class HPController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (slider.value <= 0)
+        {
+            SceneManager.LoadScene("Scene1");
+        }
     }
 
     // HPを減らす処理
